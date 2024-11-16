0d4a0b2d846752eae40799dd10ab93f0e28574dc
Chris Liu <943678231@qq.com>
2024-11-16 23:51:04 +0800

    Refactor random noise scaling in fully fused projection packed backward kernel for improved clarity

--- gsplat/cuda/csrc/fully_fused_projection_packed_2dgs_bwd.cu
+++ gsplat/cuda/csrc/fully_fused_projection_packed_2dgs_bwd.cu
@@ -130,13 +130,17 @@ __global__ void fully_fused_projection_packed_bwd_2dgs_kernel(
     mat3<T> R_gs = quat_to_rotmat(quat);
     v_scale[0] += glm::dot(v_sample, R_gs[0] * randn[0]);
     v_scale[1] += glm::dot(v_sample, R_gs[1] * randn[1]);
+    vec2<T> srandn = randn * scale;
     mat3<T> v_R = mat3<T>(
-        v_sample[0] * scale[0] * randn[0],
-        v_sample[1] * scale[0] * randn[0],
-        v_sample[2] * scale[0] * randn[0],
-        v_sample[0] * scale[1] * randn[1],
-        v_sample[1] * scale[1] * randn[1],
-        v_sample[2] * scale[1] * randn[1],
+    // First column
+        v_sample[0] * srandn[0],
+        v_sample[1] * srandn[0],
+        v_sample[2] * srandn[0],
+    // Second column
+        v_sample[0] * srandn[1],
+        v_sample[1] * srandn[1],
+        v_sample[2] * srandn[1],
+    // Third column
         0.0,
         0.0,
         0.0
